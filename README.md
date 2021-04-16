40ecd750262170e269f803e93dbe72e02b17be0a
BigHealsInc <bighealsinc@gmail.com>
2021-04-16 14:17:09 -0400

    new mod init

---
+++ README.md
@@ -0,0 +1,49 @@
+# Solasta Expanded Weapon Ability Scores
+
+Adds new Weapon Properties similar to Finesse that allows non-STR non-DEX Ability Score +hit +damage to be used on melee and ranged attacks. 
+
+Weapons tagged with the following properties will either use STR/DEX as melee/ranged normally or the specific stat tied to the weapon property:
+
+* Knowledge = INT
+* Intuition = WIS
+* Vigor = CON
+* Influence = CHA
+
+The current weapon list for these new properties:
+
+Knowledge Weapons:
+Dagger
+Quarterstaff
+Sling*
+Short Sword
+
+Intuition Weapons:
+Club
+Greatclub*
+Mace
+Spear
+Light hammer*
+
+Vigor Weapons:
+Flail
+Morningstar
+War Pick
+
+Influence Weapons:
+Trident*
+Whip*
+Sickle*
+
+* Not in Solasta base game, added as part of this mod, but will use visuals of other similar items.
+
+# How to Install
+
+1. Download and install [Unity Mod Manager](https://www.nexusmods.com/site/mods/21)
+2. Download the SolastaApiMod https://www.nexusmods.com/solastacrownofthemagister/mods/48/
+3. Download this mod (SolastaExpandedWeaponAbilityScores.zip)
+4. Execute Unity Mod Manager, Set Solasta Location, and Click Install
+5. Select Mods tab, drag and drop the downloaded zip files to load
+
+# Known Issues
+
+*
---
+++ SolastaExpandedWeaponAbilityScores/Main.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using UnityModManagerNet;
+using HarmonyLib;
+using I2.Loc;
+using Newtonsoft.Json.Linq;
+using SolastaModApi;
+
+namespace SolastaDMUnlocked
+{
+    public class Main
+    {
+        // [System.Diagnostics.Conditional("DEBUG")]
+        public static void Log(string msg)
+        {
+            if (logger != null) logger.Log(msg);
+        }
+
+        public static void Error(Exception ex)
+        {
+            if (logger != null) logger.Error(ex.ToString());
+        }
+
+        public static void Error(string msg)
+        {
+            if (logger != null) logger.Error(msg);
+        }
+
+        public static UnityModManager.ModEntry.ModLogger logger;
+        public static bool enabled;
+
+        public static void LoadTranslations()
+        {
+            var languageSourceData = LocalizationManager.Sources[0];
+            var translations = JObject.Parse(File.ReadAllText(UnityModManager.modsPath + @"/SolastaDMUnlocked/Translations.json"));
+            foreach (var translationKey in translations)
+            {
+                foreach (var translationLanguage in (JObject)translationKey.Value)
+                {
+                    var languageIndex = languageSourceData.GetLanguageIndex(translationLanguage.Key);
+                    if (languageIndex > 0)
+                    {
+                        languageSourceData.AddTerm(translationKey.Key).Languages[languageIndex] = translationLanguage.Value.ToString();
+                    }
+                }
+            }
+        }
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                logger = modEntry.Logger;
+
+                ModBeforeDBReady();
+
+                var harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Error(ex);
+                throw;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(MainMenuScreen), "RuntimeLoaded")]
+        static class MainMenuScreen_RuntimeLoaded_Patch
+        {
+            static void Postfix()
+            {
+                ModAfterDBReady();
+            }
+        }
+
+        // ENTRY POINT IF YOU NEED SERVICE LOCATORS ACCESS
+        static void ModBeforeDBReady()
+        {
+            LoadTranslations();
+        }
+
+        // ENTRY POINT IF YOU NEED SAFE DATABASE ACCESS
+        static void ModAfterDBReady()
+        {
+            UnlockEnemies();
+            UnlockTraps();
+            UnlockItems();
+            //UnlockLootPacks();
+        }
+
+        private static void UnlockEnemies()
+        {
+            // Should I address issue of certain monsters not having useful AI, like City Guards (defaultBattleDecisionPackage)
+            // Some monsters are causing a crash too, I should protect against this a bit - really seems to just be the Gargoyle which is missing its asset?
+            MonsterDefinition[] monster_definitions = DatabaseRepository.GetDatabase<MonsterDefinition>().GetAllElements();
+            foreach (MonsterDefinition monster_definition in monster_definitions)
+            {
+                Traverse.Create((object)monster_definition).Field("inDungeonEditor").SetValue((object)true);
+            }
+        }
+
+        private static void UnlockLootPacks()
+        {
+            // Maybe not necessary, the lootpacks are all missing their GUI representation and don't offer much, would rather have a mod dedicated to DM loot packs
+            throw new NotImplementedException();
+        }
+
+        private static void UnlockItems()
+        {
+            ItemDefinition[] item_definitions = DatabaseRepository.GetDatabase<ItemDefinition>().GetAllElements();
+            foreach (ItemDefinition item_definition in item_definitions)
+            {
+                Traverse.Create((object)item_definition).Field("inDungeonEditor").SetValue((object)true);
+            }
+        }
+
+        private static void UnlockTraps()
+        {
+            String description;
+            String title;
+            EnvironmentEffectDefinition[] env_effect_definitions = DatabaseRepository.GetDatabase<EnvironmentEffectDefinition>().GetAllElements();
+            foreach (EnvironmentEffectDefinition env_effect_definition in env_effect_definitions)
+            {
+                description = env_effect_definition.GuiPresentation.Description;
+                title = env_effect_definition.GuiPresentation.Title;
+                if (title == "")
+                {
+                    title = env_effect_definition.name;
+                    title = title.Replace("_", " ");
+                }
+                if (description == "")
+                {
+                    description = env_effect_definition.name;
+                    description = description.Replace("_", " ");
+                }
+                GuiPresentationBuilder presentationBuilder = 
+                new GuiPresentationBuilder(description, title);
+                GuiPresentation guiPresentation = presentationBuilder.Build();
+                Traverse.Create((object)env_effect_definition).Field(nameof(guiPresentation)).SetValue((object)guiPresentation);
+                Traverse.Create((object)env_effect_definition).Field("inDungeonEditor").SetValue((object)true);
+            }
+        }
+    }
+}
